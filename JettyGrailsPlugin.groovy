48eb274986d4212765ba45739b31d7110657d0e1
graeme <graeme@832c1c66-4827-0410-8465-ccb17913cd09>
2009-07-16 14:17:41 +0000

    Initial release of Jetty plugin for Grails
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-jetty/trunk@54179 832c1c66-4827-0410-8465-ccb17913cd09

---
+++ JettyGrailsPlugin.groovy
@@ -0,0 +1,21 @@
+class JettyGrailsPlugin {
+    // the plugin version
+    def version = "1.0"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.1 > *"
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
+
+    def author = "Graeme Rocher"
+    def authorEmail = "graeme.rocher@springsource.com"
+    def title = "Jetty Plugin"
+    def description = '''\\
+Makes Jetty 6.1.14 the development time container for Grails
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/jetty"
+
+}
