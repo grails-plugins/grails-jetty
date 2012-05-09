c56cd2d970f3ad60b77df7f4e8273897b2dc86c3
Burt Beckwith <beckwithb@vmware.com>
2012-05-09 00:45:30 -0400

    update to 2.0 for dev

--- JettyGrailsPlugin.groovy
+++ JettyGrailsPlugin.groovy
@@ -1,23 +1,30 @@
+/* Copyright 2012 SpringSource.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 class JettyGrailsPlugin {
-    // the plugin version
-    def version = "1.2-SNAPSHOT"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.2 > *"
-	// scope
-	def scope = ['dev']
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
+	def version = "2.0.0.BUILD-SNAPSHOT"
+	def grailsVersion = "2.0 > *"
+	def scopes = [excludes: 'war']
+	def title = "Jetty Plugin"
+	def author = "Graeme Rocher"
+	def authorEmail = "graeme.rocher@springsource.com"
+	def description = 'Makes Jetty the development time container for Grails'
+	def documentation = "http://grails.org/plugin/jetty"
 
-    def author = "Graeme Rocher"
-    def authorEmail = "graeme.rocher@springsource.com"
-    def title = "Jetty Plugin"
-    def description = '''\\
-Makes Jetty the development time container for Grails
-'''
-
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/jetty"
-
+	def license = 'APACHE'
+	def organization = [name: 'SpringSource', url: 'http://www.springsource.org/']
+	def developers = [[name: 'Burt Beckwith', email: 'beckwithb@vmware.com']]
+	def issueManagement = [system: 'JIRA', url: 'http://jira.grails.org/browse/GPJETTY']
+	def scm = [url: 'https://github.com/grails-plugins/grails-jetty']
 }
---
+++ grails-app/conf/Config.groovy
@@ -0,0 +1,3 @@
+grails.doc.authors = 'Graeme Rocher, Burt Beckwith'
+grails.doc.license = 'Apache License 2.0'
+grails.doc.title = 'Jetty Plugin'
