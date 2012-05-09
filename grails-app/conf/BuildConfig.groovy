b0ab5f5bbb8d9f504056d320570179621986b8a3
Burt Beckwith <beckwithb@vmware.com>
2012-05-09 01:04:59 -0400

    updated excludes

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -20,19 +20,22 @@ grails.project.dependency.resolution = {
 	dependencies {
 		String jettyVersion = '8.1.3.v20120416'
 		compile("org.eclipse.jetty:jetty-server:$jettyVersion",
-				  "org.eclipse.jetty:jetty-webapp:${jettyVersion}",
-				  "org.eclipse.jetty:jetty-http:${jettyVersion}",
-				  "org.eclipse.jetty:jetty-servlet:${jettyVersion}",
-				  "org.eclipse.jetty:jetty-security:${jettyVersion}",
+		        "org.eclipse.jetty:jetty-webapp:${jettyVersion}",
+		        "org.eclipse.jetty:jetty-http:${jettyVersion}",
+		        "org.eclipse.jetty:jetty-servlet:${jettyVersion}",
+		        "org.eclipse.jetty:jetty-security:${jettyVersion}",
 		        "org.eclipse.jetty:jetty-plus:$jettyVersion",
 		        "org.eclipse.jetty:jetty-util:$jettyVersion",
 		        "org.eclipse.jetty:jetty-jndi:$jettyVersion",
-				  "org.eclipse.jetty:jetty-continuation:${jettyVersion}",
-				  "org.eclipse.jetty:jetty-websocket:${jettyVersion}",
+		        "org.eclipse.jetty:jetty-continuation:${jettyVersion}",
+		        "org.eclipse.jetty:jetty-websocket:${jettyVersion}",
 		        "org.eclipse.jetty:jetty-jsp:$jettyVersion") {
-			transitive = false
-//			excludes 'javax.servlet', 'junit', 'commons-el', 'ant', 'slf4j-api', 'slf4j-simple', 'jcl104-over-slf4j',
-//			         'xercesImpl', 'xmlParserAPIs'
+			excludes 'ant', 'com.sun.el', 'commons-el', 'derby', 'javax.activation', 'javax.el',
+			         'javax.mail.glassfish', 'javax.servlet', 'javax.servlet.jsp', 'javax.servlet.jsp.jstl',
+			         'jcl104-over-slf4j', 'jetty-jmx', 'jetty-io', 'jetty-test-helper', 'jetty-xml',
+			         'junit', 'mockito-core', 'org.eclipse.jdt.core', 'org.apache.jasper.glassfish',
+			         'org.apache.taglibs.standard.glassfish', 'slf4j-api', 'slf4j-jdk14',
+			         'slf4j-simple', 'xercesImpl', 'xmlParserAPIs'
 		}
 	}
 }
