1f01473c115f3b88e18d85e426322792287e32b9
Burt Beckwith <beckwithb@vmware.com>
2012-05-09 13:51:57 -0400

    updated excludes

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -33,19 +33,7 @@ grails.project.dependency.resolution = {
 			excludes 'ant', 'commons-el', 'derby', 'jcl104-over-slf4j', 'jetty-jmx',
 			         'jetty-io', 'jetty-test-helper', 'jetty-xml', 'junit', 'mockito-core',
 			         'slf4j-api', 'slf4j-jdk14', 'slf4j-simple', 'xercesImpl'
-			excludes(group: 'org.eclipse.jetty.orbit', name: 'jetty-orbit')
-
-			excludes(group: 'org.eclipse.jetty.orbit', name: 'com.sun.el')
-			excludes(group: 'org.eclipse.jetty.orbit', name: 'javax.activation')
-			excludes(group: 'org.eclipse.jetty.orbit', name: 'javax.el')
-			excludes(group: 'org.eclipse.jetty.orbit', name: 'javax.mail.glassfish')
-			excludes(group: 'org.eclipse.jetty.orbit', name: 'javax.servlet')
-			excludes(group: 'org.eclipse.jetty.orbit', name: 'javax.servlet.jsp')
-			excludes(group: 'org.eclipse.jetty.orbit', name: 'javax.servlet.jsp.jstl')
-			excludes(group: 'org.eclipse.jetty.orbit', name: 'javax.transaction')
-			excludes(group: 'org.eclipse.jetty.orbit', name: 'org.apache.jasper.glassfish')
-			excludes(group: 'org.eclipse.jetty.orbit', name: 'org.apache.taglibs.standard.glassfish')
-			excludes(group: 'org.eclipse.jetty.orbit', name: 'org.eclipse.jdt.core')
+			excludes(group: 'org.eclipse.jetty.orbit', ext: 'orbit')
 		}
 	}
 }
