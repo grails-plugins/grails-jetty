badfc424b62e9eda97db524f773c8983a8b64e96
Burt Beckwith <beckwithb@vmware.com>
2012-05-30 10:54:48 -0400

    updated dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,31 +9,76 @@ grails.project.dependency.resolution = {
 
 	repositories {
 		grailsCentral()
+		mavenLocal()
+		mavenCentral()
 	}
 
 	plugins {
-		build(':release:2.0.0', ':rest-client-builder:1.0.2') {
+		build(':release:2.0.2', ':rest-client-builder:1.0.2') {
 			export = false
 		}
 	}
 
 	dependencies {
 		String jettyVersion = '8.1.3.v20120416'
-		compile("org.eclipse.jetty:jetty-continuation:${jettyVersion}",
-		        "org.eclipse.jetty:jetty-http:${jettyVersion}",
-		        "org.eclipse.jetty:jetty-jndi:$jettyVersion",
-		        "org.eclipse.jetty:jetty-jsp:$jettyVersion",
-		        "org.eclipse.jetty:jetty-plus:$jettyVersion",
-		        "org.eclipse.jetty:jetty-security:${jettyVersion}",
-		        "org.eclipse.jetty:jetty-server:$jettyVersion",
-		        "org.eclipse.jetty:jetty-servlet:${jettyVersion}",
-		        "org.eclipse.jetty:jetty-util:$jettyVersion",
-		        "org.eclipse.jetty:jetty-webapp:${jettyVersion}",
-				  "org.eclipse.jetty:jetty-websocket:${jettyVersion}") {
-			excludes 'ant', 'commons-el', 'derby', 'jcl104-over-slf4j', 'jetty-jmx',
-			         'jetty-io', 'jetty-test-helper', 'jetty-xml', 'junit', 'mockito-core',
-			         'slf4j-api', 'slf4j-jdk14', 'slf4j-simple', 'xercesImpl'
-			exclude(group: 'org.eclipse.jetty.orbit', ext: 'orbit')
+
+		compile("org.eclipse.jetty:jetty-continuation:$jettyVersion") {
+			excludes 'javax.servlet'
 		}
+
+		compile("org.eclipse.jetty:jetty-http:$jettyVersion") {
+			excludes 'junit', 'javax.servlet'
+		}
+
+		compile("org.eclipse.jetty:jetty-io:$jettyVersion") {
+			excludes 'jetty-test-helper'
+		}
+
+		compile("org.eclipse.jetty:jetty-jmx:$jettyVersion") {
+			excludes 'junit'
+		}
+
+		compile("org.eclipse.jetty:jetty-jndi:$jettyVersion") {
+			excludes 'javax.mail.glassfish', 'junit'
+		}
+
+		compile("org.eclipse.jetty:jetty-jsp:$jettyVersion") {
+			excludes 'com.sun.el', 'javax.el', 'javax.servlet.jsp',
+			         'javax.servlet.jsp.jstl', 'org.apache.jasper.glassfish',
+			         'org.apache.taglibs.standard.glassfish', 'org.eclipse.jdt.core'
+		}
+
+		compile("org.eclipse.jetty:jetty-plus:$jettyVersion") {
+			excludes 'derby', 'javax.servlet', 'javax.transaction', 'jetty-webapp', 'junit'
+		}
+
+		compile("org.eclipse.jetty:jetty-security:$jettyVersion") {
+			excludes 'junit', 'javax.servlet'
+		}
+
+		compile("org.eclipse.jetty:jetty-server:$jettyVersion") {
+			excludes 'junit', 'javax.servlet', 'jetty-test-helper', 'mockito-core'
+		}
+
+		compile("org.eclipse.jetty:jetty-servlet:$jettyVersion") {
+			excludes 'jetty-test-helper'
+		}
+
+		compile("org.eclipse.jetty:jetty-util:$jettyVersion") {
+			excludes 'javax.servlet', 'jetty-test-helper', 'junit', 'slf4j-api', 'slf4j-jdk14'
+		}
+
+		compile "org.eclipse.jetty:jetty-webapp:$jettyVersion"
+
+		compile("org.eclipse.jetty:jetty-websocket:$jettyVersion") {
+			excludes 'javax.servlet', 'jetty-test-helper'
+		}
+
+		compile("org.eclipse.jetty:jetty-xml:$jettyVersion") {
+			excludes 'junit'
+		}
+
+		// needed for JSP compilation
+		runtime 'org.eclipse.jdt.core.compiler:ecj:3.6.2'
 	}
 }
