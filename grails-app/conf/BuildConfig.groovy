2f340d5e437c62fb92a94dab4f7fbe883ef3d22a
Burt Beckwith <beckwithb@vmware.com>
2012-06-01 01:17:02 -0400

    cleanup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,9 +1,3 @@
-import org.apache.ivy.core.module.descriptor.DefaultExcludeRule
-import org.apache.ivy.core.module.id.ArtifactId
-import org.apache.ivy.core.module.id.ModuleId
-import org.apache.ivy.core.module.id.ModuleRevisionId
-import org.apache.ivy.plugins.matcher.ExactPatternMatcher
-
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual' // for gh-pages branch
 grails.project.source.level = 1.6
@@ -26,14 +20,14 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
+
 		String jettyVersion = '7.6.0.v20120127'
-		runtime "org.eclipse.jetty.aggregate:jetty-all:$jettyVersion",{
+
+		runtime("org.eclipse.jetty.aggregate:jetty-all:$jettyVersion") {
 			transitive = false
 		}
 
 		// needed for JSP compilation
-		runtime 'org.eclipse.jdt.core.compiler:ecj:3.6.2',{
-			transitive = false
-		}
+		runtime 'org.eclipse.jdt.core.compiler:ecj:3.6.2'
 	}
 }
