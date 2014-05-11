8d5c865ade467f3aa58de6481a7bde4b9d963f56
Graeme Rocher <graeme.rocher@gmail.com>
2014-05-11 17:46:32 +0200

    Add Travis CI

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,13 @@
+if(System.getenv('TRAVIS_BRANCH')) {
+    grails.project.repos.grailsCentral.username = System.getenv("GRAILS_CENTRAL_USERNAME")
+    grails.project.repos.grailsCentral.password = System.getenv("GRAILS_CENTRAL_PASSWORD")    
+}
+
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual' // for gh-pages branch
 grails.project.source.level = 1.6
 
+grails.project.dependency.resolver='maven'
 grails.project.dependency.resolution = {
 
 	inherits 'global'
@@ -21,16 +27,14 @@ grails.project.dependency.resolution = {
             exclude "javax.servlet"
         }
 
-        runtime ("org.eclipse.jetty.orbit:javax.servlet:3.0.0.v201112011016")  {
-            addDependencyArtifact("*", new org.apache.ivy.core.module.descriptor.DefaultDependencyArtifactDescriptor(delegate, "jive", "jar", "jar", null, null))
-        }
+        runtime ("org.eclipse.jetty.orbit:javax.servlet:3.0.0.v201112011016")  
 
 		// needed for JSP compilation
 		runtime 'org.eclipse.jdt.core.compiler:ecj:3.7.2'
 	}
 
 	plugins {
-		build(':release:2.2.0', ':rest-client-builder:1.0.3') {
+		build(':release:3.0.1', ':rest-client-builder:2.0.1') {
 			export = false
 		}
 	}
