ee74010645e6ebed9046cb863796397073f1b831
graemerocher <graeme.rocher@gmail.com>
2014-05-19 14:58:15 +0200

    Release 3.0.0

--- JettyGrailsPlugin.groovy
+++ JettyGrailsPlugin.groovy
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 class JettyGrailsPlugin {
-	def version = "3.0.0-SNAPSHOT"
+	def version = "3.0.0"
 	def grailsVersion = "2.3.7 > *"
 	def scopes = [excludes: 'war']
 	def title = "Jetty Plugin"
